1bb405adac5cc70170b992a1fe068a6cd7b8a7ac
SSholtys <sergiy.sholtys@gmail.com>
2020-09-08 13:26:21 +0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { AboutExtraComponent } from './about-extra/about-extra.component';
+import { PostComponent } from './post/post.component';
+import { PostsComponent } from './posts/posts.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AuthGuard } from './auth.guard';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'about', component: AboutComponent, canActivateChild: [AuthGuard], children: [
+    {path: 'extra', component: AboutExtraComponent}
+  ] },
+  { path: 'posts', component: PostsComponent, canActivate: [AuthGuard] },
+  { path: 'posts/:id', component: PostComponent },
+  { path: 'error', component: ErrorPageComponent },
+  { path: '**', redirectTo: 'error' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
---
+++ src/app/post/post.component.ts
@@ -0,0 +1,30 @@
+import { PostsService, Post } from './../posts.service';
+import {Component, OnInit} from '@angular/core'
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss']
+})
+export class PostComponent implements OnInit {
+
+  post: Post
+
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      console.log('Params', params)
+      this.post = this.postsService.getById(+params.id)
+    })
+  }
+
+  loadPost() {
+    this.router.navigate(['/posts', 44])
+  }
+}
